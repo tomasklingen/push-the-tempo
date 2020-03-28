96293bfc65042ae10060735ddda965bee0690b30
Tomas Klingen <tomas.klingen@gmail.com>
2020-03-28 19:37:32 +0100

    Updates

---
+++ precache-manifest.9d351f35bf9372e142db9f69289d04b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255755ebcaba8b81c4795b8782344afa",
+    "url": "/push-the-tempo/index.html"
+  },
+  {
+    "revision": "7ad1b067678436172748",
+    "url": "/push-the-tempo/static/css/main.9f0ef91c.chunk.css"
+  },
+  {
+    "revision": "ed210633947253c4a2b6",
+    "url": "/push-the-tempo/static/js/2.0eb92bdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/push-the-tempo/static/js/2.0eb92bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad1b067678436172748",
+    "url": "/push-the-tempo/static/js/main.a09f7740.chunk.js"
+  },
+  {
+    "revision": "a975973fa2e1960e3e20",
+    "url": "/push-the-tempo/static/js/runtime-main.2cf5e53f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e0d3aa36dc7cf4988c321ccb72531b5.js"
+  "/push-the-tempo/precache-manifest.9d351f35bf9372e142db9f69289d04b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/push-the-tempo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
